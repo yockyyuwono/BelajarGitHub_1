07e064f8a56032225a77031563fb735130ada158
YockyYuwono <yockyyuwono@gmail.com>
2020-07-06 11:49:35 +0700

    Percobaan Commit pertama

---
+++ BelajarGitHub_1/Controllers/Coba1Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BelajarGitHub_1.Controllers
+{
+    public class Coba1Controller : Controller
+    {
+        // GET: Coba1
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
